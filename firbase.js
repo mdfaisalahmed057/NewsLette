2e56ac7c8bee512e6fe4eb97713bb242d87e2fbe
Your Name <you@example.com>
2022-03-17 16:46:22 -0500

    third

---
+++ firbase.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBZD1oJb6WeRyy17nEbw-WVjUwGvP0zFo4",
+    authDomain: "newsletter-9d5a5.firebaseapp.com",
+    projectId: "newsletter-9d5a5",
+    storageBucket: "newsletter-9d5a5.appspot.com",
+    messagingSenderId: "497798152173",
+    appId: "1:497798152173:web:2d247ecffccfd09bf9f1e4",
+    measurementId: "G-Q02CJT91FF"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+let db= firebase.firestore()
---
+++ news.js
@@ -0,0 +1,49 @@
+function getItems(){
+    db.collection("items").get().then((querySnapshot)=>{
+        let item=[]
+        querySnapshot.forEach((doc) => {
+            // console.log(`${doc.id})=>$(doc.data()`);
+            item.push({
+                id:doc.id,
+                image:doc.data().image,
+                date:doc.data().date,
+                description:doc.data().description
+
+            });
+            console.log(item)
+            
+        });
+        generateItems(item)
+
+    });
+}
+
+function generateItems(item){
+    let itemHTML=""
+    item.forEach((item)=>{
+        itemHTML+=`
+        <div class="column">
+    <div class="card">
+
+        <div class="image">
+            <img src="${item.image}"
+            style="width: 100%">
+        </div>
+
+       <div class="para">
+        ${item.description}
+       </div>
+       <div class="date">
+        ${item.date}
+       </div>
+    
+       
+    </div>
+  </div>
+        `
+
+    });
+
+    document.querySelector(".main-section").innerHTML=itemHTML
+}
+getItems()
